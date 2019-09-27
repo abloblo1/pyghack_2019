35b1ef613b53ba3cf9cca56771889e08345e31d7
Andrew Chao <abloblo1@wirelessprv-10-193-115-219.near.illinois.edu>
2019-09-27 18:57:28 -0500

    test hello world

---
+++ hello_world/hello_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello_world
+//
+//  Created by Andrew Chao on 9/27/19.
+//  Copyright © 2019 Andrew Chao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
